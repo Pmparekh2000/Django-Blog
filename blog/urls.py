68719db9553b0bebf47fb1ffc92586f7f2728826
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-11-28 16:08:03 +0530

    Added class based views

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from blog.views import PostListView
 from django.urls import path
 from . import views
 
@@ -6,7 +7,8 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
     # The above int and slug parameters are called as path converters
     # The name parameter allows us to name the URL project-wide.
